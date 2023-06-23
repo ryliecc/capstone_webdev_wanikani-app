44fb7cb9907d98425b494ab7825c69baa9b16ee5
Rylie Castell <rylie.castell@gmail.com>
2023-06-23 12:29:02 +0200

    add list component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import exampleData from "../src/data.js";
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
+  const VocabList = exampleData.map((item) => {
+    const key = item.id;
+    const characters = item.data.characters;
+    const readings = item.data.readings.map((reading) => {
+      return { reading };
+    });
+    const meanings = item.data.meanings.map((meaning) => {
+      return { meaning };
+    });
+    return (
+      <li className="vocabulary-item" key={key}>
+        <span className="characters">{characters}</span>
+        <span className="readings">{readings}</span>
+        <span className="meanings">{meanings}</span>
+      </li>
+    );
+  });
+
+  return <ul>{VocabList}</ul>;
 }

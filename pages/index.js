b1a756253b17b7196e41e68c5e56eb08b4c6acdd
Rylie Castell <rylie.castell@gmail.com>
2023-06-26 14:13:13 +0200

    add link

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import VocabularyList from "@/components/VocabList.js";
-
 export default function HomePage() {
   return (
     <>
-      <VocabularyList />
-
       <a href="/radicals">Radicals</a>
       <a href="/kanji">Kanji</a>
+      <a href="/vocabulary">Vocabulary</a>
     </>
   );
 }

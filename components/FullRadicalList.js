b29911b381b09d6e254fadd726543b76cf36c1ae
Rylie Castell <rylie.castell@gmail.com>
2023-06-26 11:13:57 +0200

    add radical list component

---
+++ components/FullRadicalList.js
@@ -0,0 +1,39 @@
+import { exampleData } from "../src/data.js";
+
+const RadicalData = exampleData.filter((item) => item.type === "radical");
+
+const RadicalHeader = styled.h2`
+  text-align: center;
+`;
+
+const RadicalList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
+
+const RadicalListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const RadicalListItemSpan = styled.span`
+  width: 50%;
+  border: 1px solid black;
+  text-align: center;
+`;
+
+const FullRadicalList = () => {
+  return (
+    <RadicalList>
+      {RadicalData.map((item) => (
+        <RadicalListItem key={item.id}>
+          <RadicalListItemSpan>{item.data.characters}</RadicalListItemSpan>
+          <RadicalListItemSpan>
+            {item.data.meanings.map((meaning) => meaning.meaning).join(", ")}
+          </RadicalListItemSpan>
+        </RadicalListItem>
+      ))}
+    </RadicalList>
+  );
+};

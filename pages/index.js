3fa4e754a1aa0263ccd12cc9f550082e6c55989a
Rylie Castell <rylie.castell@gmail.com>
2023-06-27 12:33:48 +0200

    add session buttons to dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import AppHeader from "../components/AppHeader";
+import SessionButtons from "../components/SessionButtons";
 import SubjectsNavBar from "../components/SubjectsNavBar";
 
 export default function HomePage() {
   return (
     <>
       <AppHeader />
+      <SessionButtons />
       <SubjectsNavBar />
     </>
   );

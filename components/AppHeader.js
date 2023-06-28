142d0ba98e171041e6bdb4767c8aacb5ddd940ea
Rylie Castell <rylie.castell@gmail.com>
2023-06-28 16:11:40 +0200

    style app header

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
 
-const Heading = styled.h1``;
+const Heading = styled.h1`
+  text-align: center;
+  font-size: 3rem;
+  background-color: #f4f4f4;
+  border-bottom: 1px solid #d5d5d5;
+`;
 
-const HeaderLink = styled.a``;
+const HeaderLink = styled.a`
+  text-decoration: none;
+  color: #ff00aa;
+  &visited: none;
+`;
 
 export default function AppHeader() {
   return (

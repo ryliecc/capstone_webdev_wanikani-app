a9c16768c84e809e73e7f6b10677d1ffd6f2ed9a
Rylie Castell <rylie.castell@gmail.com>
2023-07-15 01:24:30 +0200

    styling

--- components/KanjiMeaning.js
+++ components/KanjiMeaning.js
@@ -1,16 +1,32 @@
 import styled from "styled-components";
 import useSubjects from "../swr/useSubjects.js";
 
-const Heading = styled.h3``;
+const Heading = styled.h3`
+  font-size: 1.6em;
+  margin: 0.4em;
+  margin-bottom: 0;
+`;
 const Paragraph = styled.p`
   display: flex;
-  justify-content: flex-start;
   gap: 0.4em;
+  font-size: 1.2em;
+  padding: 0.4em;
 `;
-const CategorySpan = styled.span``;
-const MeaningSpan = styled.span``;
-const Subheading = styled.h4``;
-const HintsContainer = styled.section``;
+const CategorySpan = styled.span`
+  color: #999;
+`;
+const MeaningSpan = styled.span`
+  color: #333;
+`;
+const Subheading = styled.h4`
+  font-size: 1.2em;
+  padding-left: 0.6em;
+`;
+const HintsContainer = styled.section`
+  background-color: #f4f4f4;
+  margin: 1em;
+  padding: 1em;
+`;
 
 export default function KanjiMeaning({ id }) {
   const { subjects, isLoading, isError } = useSubjects(id);
